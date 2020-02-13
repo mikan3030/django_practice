71a8811ce31c997808d39b7dc6e5d37712f29175
mkant <m.kanta621@gmail.com>
2020-02-13 12:29:01 +0900

    practice of roop

--- django_practice/django_practice/views.py
+++ django_practice/django_practice/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html",
-    {"title":"Hllo World"})
+    {"title":"Hllo World","l":["ok","kusa","nashi"]})
 
 def hoge(request):
     if request.method == "POST":
@@ -28,3 +28,19 @@ def login(request):
         return render(request,"check.html",{"email":request.POST["email"],"username":request.POST["username"]})
     else:
         return render(request,"error.html")
+
+def upload(request):
+    if (request.method=="POST" 
+        and request.FILES
+        and request.FILES["image"]):
+        binary = request.FILES["image"]
+        image = open("static/hoge.png","wb")
+        for chunk in binary.chunks():
+            image.write(chunk)
+        return render(request,"result.html")
+    else:
+        # return HttpResponseRedirect("/form")
+        return render(request,"form.html")
+
+def form(request):
+    return render(request,"form.html")
